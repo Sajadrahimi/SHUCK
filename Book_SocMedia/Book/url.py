26268793f8aed4091217e1d3c02e45dea41070ed
Sajad Rahimi <sajad.rahimi75@gmail.com>
2017-10-11 22:30:57 +0330

    project renamed

--- Book_SocMedia/Book/url.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^addBook$', views.AddBook),
-    url(r'^authorBooks/(?P<AuthorName>\w{0,50})/$', views.showAuthorBooks),
-]
